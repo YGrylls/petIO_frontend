377f15bf6ad4554e3b16b86f61a8156a0cc12660
zw6666 <2714709357@qq.com>
2019-05-29 08:50:05 +0800

    111

--- src/components/nav-menu.vue
+++ src/components/nav-menu.vue
@@ -8,16 +8,17 @@
         <!--<el-menu-item index="1" @click="gotoPersonal">个人中心</el-menu-item>-->
         <el-menu-item index="2" @click="gotoAdoptionList">领养</el-menu-item>
         <el-menu-item index="3" @click="gotoAdoptionPublish">送养</el-menu-item>
-        <el-submenu index="1" id="personal">
+        <el-menu-item v-if="loginStatus==400" index="1" id="personal" @click="gotoPersonal">登录</el-menu-item>
+        <el-submenu v-else index="1" id="personal">
             <template slot="title">个人中心</template>
             <el-menu-item index="1-1" @click="gotoPersonal">
                 评论
-                <el-badge :value="12" :max="99" class="item">
+                <el-badge :value="commentUnread" :max="99" class="item">
                 </el-badge>
             </el-menu-item>
             <el-menu-item index="1-2" @click="gotoPersonal">
                 申请
-                <el-badge :value="13" :max="99" class="item">
+                <el-badge :value="applyUnread" :max="99" class="item">
                 </el-badge>
             </el-menu-item>
         </el-submenu>
@@ -29,13 +30,40 @@
         name: "nav-menu",
         data(){
             return{
-                activeIndex1:''
+                activeIndex1:'',
+                loginStatus:0,
+                applyUnread:0,
+                commentUnread:0
             }
         },
         created(){
             this.activeIndex1=this.getIndex();
+            this.getUnread();
         },
         methods:{
+            getUnread(){
+                const that=this;
+                this.$http.get("/comment/unread")
+                    .then(function (response) {
+                        that.loginStatus=response.data.code;
+                        if(that.loginStatus===200) {
+                            that.commentUnread = response.data.data.number;
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log(error)
+                    });
+                this.$http.get("/apply/unread")
+                    .then(function (response) {
+                        if(that.loginStatus==200){
+                            that.applyUnread=response.data.data.number;
+                        }
+                        console.log(response)
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    })
+            },
             getIndex(){
                 if(this.$route.path=="/adoption/personal"){
                     return "1";
